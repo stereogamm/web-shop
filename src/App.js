a20b0b82cc1673c291299a9fb6c71018c21e0dce
Marina Sysoliatina <stereogam@yandex.ru>
2024-10-09 16:00:55 +0200

    fix skeleton component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Header from "./Components/Header.jsx";
 import Categories from "./Components/Categories.jsx";
 import Card from "./Components/Card/Card.jsx";
 import Sort from "./Components/Sort.jsx";
-// import SkeletonCard from "./Components/Card/SkeletonCard.jsx";
+import SkeletonCard from "./Components/Card/SkeletonCard.jsx";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://6704f473031fd46a830e0b4e.mockapi.io/items")
       .then((response) => response.json())
-      .then((data) => setItems(data))
-      .catch((error) => console.error("Error is: ", error));
+      .then((data) => {
+        setItems(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error is: ", error);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -28,16 +35,20 @@ function App() {
           </div>
           <h2 className="content__title">Ctrl + Slurp + Del</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <Card
-                key={uuidv4()}
-                price={obj.price}
-                title={obj.title}
-                image={obj.image}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, index) => (
+                  <SkeletonCard key={index} />
+                ))
+              : items.map((obj) => (
+                  <Card
+                    key={uuidv4()}
+                    price={obj.price}
+                    title={obj.title}
+                    image={obj.image}
+                    sizes={obj.sizes}
+                    types={obj.types}
+                  />
+                ))}
           </div>
         </div>
       </div>
--- src/Components/Card/SkeletonCard.jsx
+++ src/Components/Card/SkeletonCard.jsx
@@ -4,18 +4,18 @@ import ContentLoader from "react-content-loader";
 const SkeletonCard = (props) => (
   <ContentLoader
     speed={3}
-    width={400}
-    height={466}
-    viewBox="0 0 480 466"
+    width={380}
+    height={760}
+    viewBox="0 0 380 760"
     backgroundColor="#ededed"
     foregroundColor="#d3cfcf"
     {...props}
   >
-    <rect x="67" y="299" rx="8" ry="8" width="302" height="26" />
-    <rect x="68" y="339" rx="8" ry="8" width="302" height="65" />
-    <rect x="66" y="6" rx="17" ry="17" width="302" height="280" />
-    <rect x="254" y="418" rx="20" ry="20" width="114" height="38" />
-    <rect x="67" y="423" rx="3" ry="3" width="94" height="32" />
+    <rect x="90" y="358" rx="4" ry="8" width="275" height="46" />
+    <rect x="90" y="431" rx="8" ry="8" width="275" height="91" />
+    <rect x="90" y="6" rx="17" ry="20" width="275" height="313" />
+    <rect x="219" y="536" rx="20" ry="20" width="145" height="46" />
+    <rect x="96" y="545" rx="1" ry="10" width="70" height="32" />
   </ContentLoader>
 );
 
